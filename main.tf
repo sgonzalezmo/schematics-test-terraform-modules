4214ec1a02d163b27bec688999c1cd21e9f799a4
Samuel Gonzalez Morro <samuel.gonzalez.m@itnow.es>
2021-04-12 11:53:58 +0200

    Use module v1.1.0 version.

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "resource_group_1" {
-  source = "git::https://github.com/sgonzalezmo/resource-group-example-module.git?ref=v1.0.0"
+  source = "git::https://github.com/sgonzalezmo/resource-group-example-module.git?ref=v1.1.0"
 
-  resource_group = "resource_group_test"
+  resource_group = "resource_group_test
+  "
 }
